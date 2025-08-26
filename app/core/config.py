8fbcf8dc31f297d7f0b8b013b2709e90e6c99648
VadimBarinov <barinovvadim87@gmail.com>
2025-08-26 17:21:48 +0400

    Fixed bug

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,9 @@ from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
 )
+from pathlib import Path
+
+BASE_URL = Path(__file__).parent.parent
 
 
 class RunConfig(BaseModel):
@@ -19,19 +22,19 @@ class ApiPrefix(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    max_overflow: int = 50,
-    pool_size: int = 10,
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 50
+    pool_size: int = 10
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=(BASE_URL / ".env.template", BASE_URL / ".env"),
         # Не важен регистр
         case_sensitive=False,
         env_nested_delimiter="__",
-        env_prefix="FASTAPI__",
+        env_prefix="APP_CONFIG__",
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
