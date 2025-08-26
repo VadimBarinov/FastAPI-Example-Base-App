321b597b76caba325eeebf3de0149b13e3beaf00
VadimBarinov <barinovvadim87@gmail.com>
2025-08-26 14:48:45 +0400

    Created database config + database helper

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    PostgresDsn
+)
 from pydantic_settings import BaseSettings
 
 
@@ -11,10 +14,18 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False,
+    echo_pool: bool = False,
+    max_overflow: int = 50,
+    pool_size: int = 10,
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    # db_url: str
+    db: DatabaseConfig
 
 
 settings = Settings()
