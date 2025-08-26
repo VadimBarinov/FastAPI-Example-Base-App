da480cd3d77f1e2944f1f8104ea140005eacd33f
VadimBarinov <barinovvadim87@gmail.com>
2025-08-26 17:24:58 +0400

    Alembic first migration

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -4,6 +4,5 @@ from core.models import Base
 
 
 class User(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
